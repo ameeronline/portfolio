9d13131581d28fb4c965faacb1e90a55f18d7fcb
Ameer <93423173+ameeronline@users.noreply.github.com>
2023-12-29 19:54:32 +0530

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,7 @@ function About() {
                 <div className="about-description">
                     <h4 className="description">
                         "Hello,<br /> I'm a tech enthusiast and a BCA graduate with expertise in JavaScript, Node.js, Express.js, and Java. Proficient in Software Development Life Cycle (SDLC), I specialize in delivering high-quality software solutions. Beyond coding, I'm also an avid Photographer & Instagrammer, sharing creative travel content. My journey as a software developer reflects not only my academic achievements but also a passion for exploring new technologies and trends."<br />
-                        <img className='signature' src="./name.png" alt="Signature Photo" />
+                        <img className='signature' src="./public/name.png" alt="Signature Photo" />
                     </h4>
                 </div>
                 <div className="about-photo">
