2c6c1bea22ea2a350b0920e891fd9925197eb618
ameeronline <helloameeronline@gmail.com>
2026-02-08 15:10:19 +0530

    Added Event Component-Valentines day

---
+++ src/Components/Events/Events.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Events.css";
+
+function Events() {
+  return (
+    <div className="event">
+      <div className="left">
+        <img
+          className="event-photo"
+          src="./photos/event-image-2.png"
+          alt="Valentine special event banner"
+        />
+      </div>
+      <div className="right">
+        <h4 className="description">
+          The Day of Love,
+          <br /> To my favourite person, <br />
+          In my story you'll always be my favourite chapter.
+          <br />
+          Let's meet again for the first time. <br /> <br />- Happy Valentines
+          Day!
+        </h4>
+        <div className="valentine-button">
+          <button onClick={() => window.open("https://be-my-valentine-phi-sooty.vercel.app/", "_blank")}
+            className="download-button col-12"
+          >
+            Be My Valentine&nbsp;<i class="bi bi-chat-heart"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Events;
